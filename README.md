bdbcf6d13f5bbeb81053c405afb983ffd09770b3
Shubham Mundhra <shubham23official@gmail.com>
2025-07-04 16:01:00 +0530

    performed EDA and created piplelines

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 This project is made with intention to learn end to end ml project with deployment
 1) src folder contains all the subfolders required to use it as a distributable package.
 2) components folder esentials devides the code into multiple components to help keep the code clean and modularised
-3)pipeline splits the code to treat training and testing separately
+3) pipeline splits the code to treat training and testing separately
 
 <h1> Exception handling </h1>
     1) Created a custom error handeler with custom error message
+<h1> Logging </h1>
+    1) any and all outcomes including error messages will be logged into log files.
--- src/exceptions.py
+++ src/exceptions.py
@@ -1,6 +1,6 @@
 import sys
 import logging
-
+from logger import logging
 def error_msg_detail(error,error_detail:sys):
     _,_,exc_tb=error_detail.exc_info()
     file_name=exc_tb.tb_frame.f_code.co_filename
@@ -16,9 +16,9 @@ class CustomException(Exception):
     #overriding string method
     def __str__(self):
         return self.error_msg
-# if __name__=="__main__":
-#     try:
-#         a=1/0
-#     except Exception as e:
-#         logging.info("Divide by zero")
-#         raise CustomException(e,sys)    
+if __name__=="__main__":
+    try:
+        a=1/0
+    except Exception as e:
+        logging.info("Not defined")
+        raise CustomException(e,sys)    
